5552b47decce79669be82415a5a2037cb76ab218
gurrium <gurrium@hatena.ne.jp>
2022-06-21 20:13:49 +0900

    Initial Commit

---
+++ SwiftChartPOC/SwiftChartPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartPOCApp.swift
+//  SwiftChartPOC
+//
+//  Created by gurrium on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
